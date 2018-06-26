7674cf95cc2a275688130dcc03b2d5a577fc0ab3
Kelvin Tan <kelvin.tyb@gmail.com>
2018-06-26 23:17:48 +0800

    removed duplicated tests in CollateralToken.test.ts

--- test/CollateralToken.test.ts
+++ test/CollateralToken.test.ts
@@ -51,16 +51,6 @@ describe('CollateralToken class', () => {
       await contractTester.assertMethod(contract.totalSupply, expected);
     });
 
-    it('has transferFrom', async () => {
-      const from = '0x74892';
-      const to = '0x3847293';
-      const value = 12829;
-
-      contractTester.setupTxMethodSpy('transferFromTx', {}, from, to, value);
-
-      await contractTester.assertTxMethod(contract.transferFromTx(from, to, value), {});
-    });
-
     it('has INITIAL_SUPPLY', async () => {
       const expected = new BigNumber(1000000);
 
@@ -73,19 +63,6 @@ describe('CollateralToken class', () => {
 
       contractTester.setupGetterSpy('decimals', expected);
       await contractTester.assertMethod(contract.decimals, expected);
-    });
-
-
-    it('has decreaseApproval', async () => {
-      const spender = '0x3847293';
-      const subtractedValue = 12890;
-
-      contractTester.setupTxMethodSpy('decreaseApprovalTx', {}, spender, subtractedValue);
-
-      await contractTester.assertTxMethod(
-        contract.decreaseApprovalTx(spender, subtractedValue),
-        {}
-      );
     });
 
     it('has balanceOf', async () => {
@@ -113,15 +90,6 @@ describe('CollateralToken class', () => {
       await contractTester.assertTxMethod(contract.transferTx(to, value), {});
     });
 
-    it('has increaseApproval', async () => {
-      const spender = '0x3847293';
-      const addedValue = 12890;
-
-      contractTester.setupTxMethodSpy('increaseApprovalTx', {}, spender, addedValue);
-
-      await contractTester.assertTxMethod(contract.increaseApprovalTx(spender, addedValue), {});
-    });
-
     it('has allowance', async () => {
       const owner = '0x7368732648';
       const spender = '0x84789372';
@@ -134,24 +102,6 @@ describe('CollateralToken class', () => {
   });
 
   describe('methods', () => {
-    it('has transfer', async () => {
-      const to = '0x3847293';
-      const value = 12890;
-
-      contractTester.setupTxMethodSpy('transferTx', {}, to, value);
-
-      await contractTester.assertTxMethod(contract.transferTx(to, value), {});
-    });
-
-    it('has approve', async () => {
-      const spender = '0x3847293';
-      const value = 121;
-
-      contractTester.setupTxMethodSpy('approveTx', {}, spender, value);
-
-      await contractTester.assertTxMethod(contract.approveTx(spender, value), {});
-    });
-
     it('has transferFrom', async () => {
       const from = '0x74892';
       const to = '0x3847293';
@@ -182,25 +132,5 @@ describe('CollateralToken class', () => {
 
       await contractTester.assertTxMethod(contract.increaseApprovalTx(spender, addedValue), {});
     });
-
-    it('has balanceOf', async () => {
-      const owner = '0x7368732648';
-      const expected = new BigNumber(203);
-
-      contractTester.setupMethodSpy('balanceOf', expected, owner);
-
-      await contractTester.assertMethod(contract.balanceOf(owner), expected);
-    });
-
-    it('has allowance', async () => {
-      const owner = '0x7368732648';
-      const spender = '0x84789372';
-      const expected = new BigNumber(102);
-
-      contractTester.setupMethodSpy('allowance', expected, owner, spender);
-
-      await contractTester.assertMethod(contract.allowance(owner, spender), expected);
-    });
   });
-
 });
