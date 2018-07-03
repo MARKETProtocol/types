84001028ad939f84db3c3f400de23c4f67065cb8
Phil Elsasser <phil@marketprotocol.io>
2018-07-03 11:41:56 -0600

    add more Errors to enum

--- types/MarketError.ts
+++ types/MarketError.ts
@@ -1,4 +1,15 @@
 export enum MarketError {
+  UnhandledError = 'UNHANDLED_ERROR',
+  UserHasNoAssociatedAddress = 'USER_HAS_NO_ASSOCIATED_ADDRESSES',
+  UserNotEnabledForContract = 'USER_NOT_ENABLED_FOR_CONTRACT',
+  InvalidSignature = 'INVALID_SIGNATURE',
+  ContractNotDeployedOnNetwork = 'CONTRACT_NOT_DEPLOYED_ON_NETWORK',
+  InsufficientAllowanceForTransfer = 'INSUFFICIENT_ALLOWANCE_FOR_TRANSFER',
+  InsufficientBalanceForTransfer = 'INSUFFICIENT_BALANCE_FOR_TRANSFER',
+  InsufficientCollateralBalance = 'INSUFFICIENT_COLLATERAL_BALANCE',
+  OutOfGas = 'OUT_OF_GAS',
+  NoNetworkId = 'NO_NETWORK_ID',
   SubscriptionNotFound = 'SUBSCRIPTION_NOT_FOUND',
-  SubscriptionAlreadyPresent = 'SUBSCRIPTION_ALREADY_PRESENT'
+  SubscriptionAlreadyPresent = 'SUBSCRIPTION_ALREADY_PRESENT',
+
 }
